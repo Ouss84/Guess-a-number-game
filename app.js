3a331e071c4aa03ba0d3db126884c0b06706f0de
Ouss Bahri <bahrioussama84@gmail.com>
2020-11-17 17:39:08 +0200

    defining the limits

--- app.js
+++ app.js
@@ -15,20 +15,5 @@ const form = document.querySelector("#form"),
 minNum.textContent = min;
 maxNum.textContent = max;
 
-//adding the event listener when submitting the choice
-form.addEventListener("submit", function (e){
-let guess = yourGuess.nodeValue;
-if(guess === null || guess > max || guess < min){
-    messageDisplay(`please enter a valid number between ${minNum} and ${maxNum}`, 'red');
-}
 
-
-    e.preventDefault();
-})
-
-//defining the messageDisplay function 
-
-function messageDisplay(message, color){
-
-}
 
