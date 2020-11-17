efdfae971c01b57bbeb7c4d7068789fd0dc8664d
Ouss Bahri <bahrioussama84@gmail.com>
2020-11-17 17:38:16 +0200

    declaring the global variables and selecting the UI variables

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const form = document.querySelector("#form");
+//declaring the variables
+let min = 1,
+    max = 10,
+    winNum = 5,
+    guessesNum = 3;
 
-form.addEventListener("submit", guessOperation);
+//UI variables
+const form = document.querySelector("#form"),
+      yourGuess =document.querySelector("#guess"),
+      minNum = document.querySelector('#min'),
+      maxNum =document.querySelector('#max'),
+      result = document.querySelector('#result');
 
-//defining the guess operation
-function guessOperation (e){
+//defining the limits
+minNum.textContent = min;
+maxNum.textContent = max;
 
-const result = document.querySelector("#result").value ="hello"
+//adding the event listener when submitting the choice
+form.addEventListener("submit", function (e){
+let guess = yourGuess.nodeValue;
+if(guess === null || guess > max || guess < min){
+    messageDisplay(`please enter a valid number between ${minNum} and ${maxNum}`, 'red');
+}
+
 
     e.preventDefault();
+})
+
+//defining the messageDisplay function 
+
+function messageDisplay(message, color){
+
 }
+
